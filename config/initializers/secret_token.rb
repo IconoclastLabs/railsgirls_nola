e46dadb171fb8d5d783f1052de515a7ddfdbf9c0
Matt Garrison <matt@iconoclastlabs.com>
2014-01-05 20:52:37 -0600

    initialized new rails project, added logo files, bundled

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsgirlsNola::Application.config.secret_key_base = '50a6b3366c6d92540d6df057a0f94313501ae910eb21d348e4726b172ffaa41ed841298c8a426c12e500e71c34c37d3e1779527daea5459c8be714e3771ec16f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsgirlsNola::Application.config.session_store :cookie_store, key: '_railsgirls_nola_session'
