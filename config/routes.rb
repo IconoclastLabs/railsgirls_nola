83632c4416f49812ce6bcbaa3bfd01fe517dce74
Matt Garrison <matt@iconoclastlabs.com>
2014-01-08 02:02:16 -0600

    working on getting Magellan sticky nav working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 RailsgirlsNola::Application.routes.draw do
   get "welcome/index"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
+  get "/resources" => "welcome#resources"
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
